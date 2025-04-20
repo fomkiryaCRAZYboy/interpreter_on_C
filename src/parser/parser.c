c41b5088971620a387df803bdf57354ecee8d079
fomkiryaCRAZYboy <kirillfominyh775@mail.ru>
2025-04-20 19:39:34 +0300

    finalize the processing of 'print' keyword in parser.c (chek the comments in parser.c)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,10 +1,121 @@
 #include "parser.h"
 #include "../lexer/lexer.h"  //для типа TOKEN
 
-#define STACK_SIZE  100 //максимальное количество выражений(структур EXPRESSION) в строке
+#include <stdio.h>
+#include <string.h>
 
-EXPRESSION* stack[STACK_SIZE];
+#define STACK_SIZE  1000 //максимальное количество выражений(структур EXPRESSION) 
+#define VARS_COUNT 100 //максимально количество хранимых в программе переменных
+
+EXPRESSION stack[STACK_SIZE] = {0};
 int expressions_count = 0;
+
+VARIABLES variable_table[VARS_COUNT] = {0};  //таблица переменных и их значений
+int variables_count = 0;  //увеличиваем на единицу при создании новой переменной
+
+
+bool variable_existence_check(char* var_name){
+    bool if_exists = false;
+    for(int i = 0; i < variables_count; i++){
+        if(strcmp(var_name, variable_table[i].name) == 0) if_exists = true;
+    }
+    return if_exists;
+}
+
+
+
+EXPRESSION* create_expression(const int line_number, const char* expr, const char* kword, const char* args){
+    EXPRESSION* new_expr = malloc(sizeof(EXPRESSION));
+    if(!new_expr){
+        printf("ERROR: Memory error\n");
+        return NULL;
+    }
+
+    new_expr -> line_number = line_number;
+
+    if(expr){
+        new_expr -> expression = malloc(strlen(expr) + 1);
+        if(!new_expr -> expression) {
+            free(new_expr);    //если не получилось выделить память под текст выражения, то освобождаем память занятую всем выражением (EXPRESSION) 
+            printf("ERROR: Memory error\n");
+            return NULL;
+        }
+
+        strcpy(new_expr -> expression, expr);
+    } else new_expr -> expression = NULL;  //если выражение было передано в функцию как NULL, то инициализируем поле структуры как NULL
+
+    if(kword){
+        new_expr -> keyword = malloc(strlen(kword) + 1);
+        if(!new_expr -> keyword) {
+            if(new_expr -> expression) free(new_expr -> expression);  // аналогично освобождаем ВСЮ выделенную память, в случае единственного неудачного выделания
+            free(new_expr);
+            printf("ERROR: Memory error\n");
+            return NULL;
+        }    
+
+        strcpy(new_expr -> keyword, kword);
+    } else new_expr -> keyword = NULL; //аналогичная инициализация NULL, чтобы не было 'мусорных' значений
+
+    if(args){
+        new_expr -> arguments = malloc(strlen(args) + 1);
+        if(!new_expr -> arguments) {
+            if(new_expr -> expression) free(new_expr -> expression);
+            if(new_expr -> keyword) free(new_expr -> keyword);
+            free(new_expr);
+            
+            printf("ERROR: Memory error\n");
+            return NULL;
+        }
+
+        strcpy(new_expr -> arguments, args);
+    } else new_expr -> arguments = NULL;
+
+    return new_expr;
+}
+
+
+
+ADD_EXPRESSION_STATUS add_expression(EXPRESSION* expr){
+    if(expressions_count >= STACK_SIZE){
+        printf("ERROR: Too many expressions");
+        return Failed_Add_Expression;
+    }
+
+    stack[expressions_count].line_number = expr->line_number;
+
+    if(expr -> expression){  //если при создании выражения, текст выражения НЕ был проинициализирован как NULL
+        stack[expressions_count].expression = malloc(strlen(expr -> expression) + 1);
+        if(!stack[expressions_count].expression) {
+            printf("ERROR: Memory error\n");
+            return Failed_Add_Expression;
+        }
+
+        strcpy(stack[expressions_count].expression, expr -> expression);
+    } else stack[expressions_count].expression = NULL;   //иначе инициализируем поле структуры 'expression' как NULL
+
+    if(expr -> keyword){  
+        stack[expressions_count].keyword = malloc(strlen(expr -> keyword) + 1);
+        if(!stack[expressions_count].keyword) {
+            printf("ERROR: Memory error\n");
+            return Failed_Add_Expression;
+        }
+
+        strcpy(stack[expressions_count].keyword, expr -> keyword);
+    } else stack[expressions_count].keyword = NULL;   
+
+    if(expr -> arguments){  
+        stack[expressions_count].arguments = malloc(strlen(expr -> arguments) + 1);
+        if(!stack[expressions_count].arguments) {
+            printf("ERROR: Memory error\n");
+            return Failed_Add_Expression;
+        }
+
+        strcpy(stack[expressions_count].arguments, expr -> arguments);
+    } else stack[expressions_count].arguments = NULL;   
+    
+    return Succsessful_Add_Expression;
+}
+
 
 
 OPERATOR_PRIORITY get_priority(TOKEN token) {
@@ -23,45 +134,38 @@ OPERATOR_PRIORITY get_priority(TOKEN token) {
     return ANOTHER;
 }
 
+
+
 PARSING_STATUS parsing(const TOKEN stream[], const int tokens_count){
     int token_idx = 0;
-    int priority = -1;
+   
+    //обработка ключевого слова print
 
-    int perantases_count = 0;
-    int comma_count = 0;
-    int mult_div_count = 0;
-    int add_sub_count = 0;
-    int assign_count = 0;
-    int semicolon_count = 0;
-
-    for(int i = 0; i < tokens_count - 1; i++) {
-        if(stream[token_idx].type == TOKEN_open_paren || stream[token_idx].type == TOKEN_close_paren){
-            perantases_count++;
+    /*учесть незначащие пробелы и подвыражения, например: x + 5.
+    сначала необходимо вычислить подвыражения, а затем добавить их в поток*/
+    if(stream[token_idx++].type == TOKEN_print) {
+        if(stream[token_idx++].type != TOKEN_open_paren){ //ключевому слову print необходимо указать аргументы в скобках
+            printf("ERROR: The 'print' keyword requires a list of arguments in parentheses\n");
+            return Failed_Parsing;
         }
-        if(stream[token_idx].type == TOKEN_comma) comma_count++;
-        if(stream[token_idx].text == "*" || stream[token_idx].text == "/") mult_div_count++;
-        if(stream[token_idx].text == "+" || stream[token_idx].text == "-") add_sub_count++;
-        if(stream[token_idx].type == TOKEN_assign) assign_count++;
-        if(stream[token_idx].type == TOKEN_semicolon) semicolon_count++;
-    }
-    
-    if(assign_count > 1) {
-        printf("ERROR: Too many assignment statements\n");
-        return Failed_Parsing;
-    }
 
-    if(perantases_count % 2 != 0){
-        printf("ERROR: The parentases must be closed\n");
-        return Failed_Parsing;
-    }
-
-    if(semicolon_count != 1 || stream[tokens_count-1].type != TOKEN_semicolon){
-        printf("ERROR: There should be ONE semicolon at the END of the line\n");
-        return Failed_Parsing;
+        if(stream[token_idx].type == TOKEN_variable){
+            //проверяем, что передання переменная существует
+            if(!(variable_existence_check(stream[token_idx].text))) {
+                printf("ERROR: Variable '%s' not declared\n", stream[token_idx].text);
+                return Failed_Parsing;
+            }
+        }
+        //create_expression(NULL, "print", )
     }
 
     token_idx = 0;
+
+    while(get_priority(stream[token_idx]) != OPEN_OR_CLOSE_PAREN){
+        token_idx++;
+    }
     
+
 
     return Successful_Parsing;
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -2,11 +2,19 @@
 #define PARSER_H
 #include "../lexer/lexer.h"  //для типа TOKEN
 
+#include <stdbool.h>
+
+
 //возвращаемое значение функции parsing
 typedef enum {
     Successful_Parsing,
-    Failed_Parsing
+    Failed_Parsing,
 } PARSING_STATUS;
+
+typedef enum {
+    Failed_Add_Expression,
+    Succsessful_Add_Expression,
+} ADD_EXPRESSION_STATUS;
 
 
 typedef enum {
@@ -35,15 +43,34 @@ typedef enum {
 
 //будет формироваться свой стек выражений
 typedef struct {
-    EXPRESSION* next_expression;  //указатель на выражение, которое следует вычислять следующим
+    int line_number;
     char* expression;  //текстовое представление выражения
+    char* keyword; //ключевые слова print, end (если их нет: NULL)
+    char* arguments; //список аргументов (если есть print, иначе - NULL)
 } EXPRESSION;
 
-//функция для анализа потока токенов и формирования выражений
+//структура, хранящая название переменной и ее значение (пока что только целочисленные значения)
+typedef struct{
+    char* name;
+    int value;
+} VARIABLES;
+
+
+//основная функция для анализа потока токенов
 PARSING_STATUS parsing(const TOKEN stream[], const int tokens_count);
+
+
+//создание выражения
+EXPRESSION* create_expression(const int line_number, const char* expr, const char* kword, const char* args);
+
+
+//добавление выражения в поток
+ADD_EXPRESSION_STATUS add_expression(EXPRESSION* expr);
+
+//проверка существования переменной в таблице
+bool variable_existence_check(char* var_name);
 
 //функция получения проиритета токена
 OPERATOR_PRIORITY get_priority(TOKEN token);
-
 
 #endif
